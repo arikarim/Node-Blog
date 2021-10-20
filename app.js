078081825229471afa63f51e24feb12e533dd5a7
ari <arikarim146@gmail.com>
2021-10-20 15:11:34 +0300

    Add blog Schema

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
-const dbURI = 'mongodb+srv://ari:password123456@cluster0.ivwqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+const dbURI =
+  "mongodb+srv://ari:password123456@cluster0.ivwqm.mongodb.net/cats?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 // listen for requests
-app.listen(3000);
 
 // register view engine
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
 
 app.use(express.static("public"));
+
+app.get("./add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "My first blog",
+    snippet: "This is my first snippet blog",
+    body: "This is my first blog",
+  });
+
+  blog.save();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
