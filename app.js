e513d9e67a9c0aac39c94439095ebbb46900aa25
ari <arikarim146@gmail.com>
2021-10-20 14:50:02 +0300

    Add mongoose

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+// express app
 const app = express();
 
-app.set("view engine", "ejs");
+const dbURI = 'mongodb+srv://ari:password123456@cluster0.ivwqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set('views', 'myviews');
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
-app.get('/cats/create', (req, res) => {
-  res.render('create');
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
+
+// 404 page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
